33d974a74ada8af3b55a3a960e31766524b80e38
Dzhalsan <erdniev08jalsan@gmail.com>
2024-01-16 13:37:30 -0500

    initial push

---
+++ src/test/java/step_definitions/DzhalsanSteps.java
@@ -0,0 +1,13 @@
+package step_definitions;
+
+import org.openqa.selenium.WebDriver;
+import pages.SalesPage;
+import utilities.Driver;
+
+public class DzhalsanSteps {
+
+    WebDriver driver = Driver.getDriver();
+
+    SalesPage salesPage = new SalesPage(driver);
+
+}
