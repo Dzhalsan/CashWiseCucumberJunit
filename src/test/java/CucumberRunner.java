95ec9b5ed22a87cd3308748127e4a3939c88ae82
Karina <karina@Karinas-MacBook-Pro-2.local>
2024-01-19 17:58:26 -0500

    Updated

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
         plugin = {"html:target/CucumberReport.html", "json:target/report.json"},
         features = "src/test/resources/features",
-        glue = "step_definition",
+        glue = "step_definitions",
         dryRun = false
 )
 
--- src/test/java/step_definitions/KarinaSteps.java
+++ src/test/java/step_definitions/KarinaSteps.java
@@ -4,8 +4,11 @@ import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import pages.KarinaPage;
 
 public class KarinaSteps {
@@ -27,53 +30,88 @@ public class KarinaSteps {
         Thread.sleep(3000);
         karinaPage.invoiceButton.click();
     }
+    @Given("user clicks to create invoice button")
+    public void user_clicks_to_create_invoice_button() {
+        karinaPage.createInvoiceButton.click();
+    }
+    @Then("user enters all the valid fields")
+    public void user_enters_all_the_valid_fields() {
+        Faker faker = new Faker();
+        karinaPage.invoiceName.sendKeys(faker.name().name());
+
+        Actions actionsTest = new Actions(driver);
+        actionsTest.click(karinaPage.whomButton).pause(500).sendKeys(Keys.ENTER).perform();
+
+
+        karinaPage.payUntil.click();
+        Actions actions = new Actions(driver);
+        actions.sendKeys("12/02/2024").perform();
+
+        karinaPage.description.click();
+        Actions actionsDescription = new Actions(driver);
+        actions.sendKeys("Invoice").perform();
+
+
+
+    }
+        @Then("user have to select product or service")
+    public void user_have_to_select_product_or_service() {
+         karinaPage.selectProductButton.click();
+         karinaPage.pickTitle.click();
+
+        }
+    @Then("user clicks to save  button")
+    public void user_clicks_to_save_button() {
+        karinaPage.saveButton.click();
+
+    }
     @Given("user clicks on Accept payment button")
     public void user_clicks_on_accept_payment_button() throws InterruptedException {
         Thread.sleep(3000);
-        KarinaPage.acceptPayment.click();
+        karinaPage.acceptPayment.click();
     }
     @Then("user click on Payment method drop-down box")
     public void user_click_on_payment_method_drop_down_box() {
-        KarinaPage.selectPaymentMethod.click();
+        karinaPage.selectPaymentMethod.click();
     }
     @Then("user clicks on bank method")
     public void user_clicks_on_bank_method() {
-        KarinaPage.bank.click();
+        karinaPage.bank.click();
     }
     @Then("user clicks to which check")
     public void user_clicks_to() {
-        KarinaPage.toWhichCheck.click();
+        karinaPage.toWhichCheck.click();
     }
     @Then("user have to create new check")
     public void user_have_to_create_new_check() {
-        KarinaPage.CreateNewCheck.click();
+        karinaPage.CreateNewCheck.click();
     }
     @Then("user enters  details into all the valid fields")
     public void user_enters_details_into_all_the_valid_fields() throws InterruptedException {
         Faker faker = new Faker();
-        KarinaPage.title.sendKeys(faker.name().title());
-        KarinaPage.balance.sendKeys("1000");
+        karinaPage.title.sendKeys(faker.name().title());
+        karinaPage.balance.sendKeys("1000");
         Thread.sleep(3000);
     }
     @Then("user clicks to save button")
     public void user_clicks_to_button() {
-        KarinaPage.SaveNewCheck.click();
+        karinaPage.SaveNewCheck.click();
     }
     @Then("user clicks to created check")
     public void user_clicks_to_crested_check() {
-        KarinaPage.paid.click();
+        karinaPage.paid.click();
 
 
     }
     @Then("user write comment")
     public void user_write_comment() {
-        KarinaPage.comment.sendKeys("Payment");
+        karinaPage.comment.sendKeys("Payment");
 
 
     }
     @Then("click to save button")
     public void click_to_button() {
-        KarinaPage.saveButton2.click();
+        karinaPage.saveButton2.click();
 
         driver.quit();
     }
