74d298170f26a595ebf27d8dd2e8b923be2bdabd
Dzhalsan <erdniev08jalsan@gmail.com>
2024-01-19 15:24:04 -0500

    all in one

--- src/test/java/step_definitions/DzhalsanSteps.java
+++ src/test/java/step_definitions/DzhalsanSteps.java
@@ -1,5 +1,6 @@
 package step_definitions;
 
+import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
 import pages.SalesPage;
 import utilities.Driver;
@@ -9,5 +10,71 @@ public class DzhalsanSteps {
     WebDriver driver = Driver.getDriver();
 
     SalesPage salesPage = new SalesPage(driver);
+
+    @Given("I am logged in")
+    public void i_am_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I click on Sales button")
+    public void i_click_on_sales_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I click on Invoice button")
+    public void i_click_on_invoice_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I click on Create invoce button")
+    public void i_click_on_create_invoce_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I put Invoice name")
+    public void i_put_invoice_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I select client in Whom field")
+    public void i_select_client_in_whom_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I put date in Pay until")
+    public void i_put_date_in_pay_until() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I put description")
+    public void i_put_description() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I select product or service")
+    public void i_select_product_or_service() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click on Save Button")
+    public void i_click_on_save_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I shoul see a success message")
+    public void i_shoul_see_a_success_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
--- src/test/resources/features/Dzhalsan.feature
+++ src/test/resources/features/Dzhalsan.feature
@@ -1,2 +1,19 @@
-Feature: Sales module - Invoice feature
-  As a user I want to be able
+#Feature: Sales module - Invoice feature
+  #As a user I want to be able to create an invoice for the client
+
+ # Scenario: Create Invoice positive
+    #Given I am logged in
+  ## Then I click on Invoice button
+    #Then I click on Create invoce button
+    #Then I put Invoice name
+    #Then I select client in Whom field
+   # Then I put date in Pay until
+    #Then I put description
+   # When I select product or service
+   # And I click on Save Button
+   # Then I shoul see a success message
+
+
+
+
+
